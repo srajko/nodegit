15abc1cde604c020b2591d21bcd40ecc1d83c994
Stjepan Rajko <stjepan.rajko@gmail.com>
2016-01-22 19:03:18 -0700

    Null-check object to lock

--- generate/templates/manual/src/lock_master.cc
+++ generate/templates/manual/src/lock_master.cc
@@ -65,7 +65,9 @@ public:
   }
 
   void ObjectToLock(const void *objectToLock) {
-    objectsToLock.insert(objectToLock);
+    if (objectToLock) {
+      objectsToLock.insert(objectToLock);      
+    }
   }
 
   void Lock(bool acquireMutexes);
