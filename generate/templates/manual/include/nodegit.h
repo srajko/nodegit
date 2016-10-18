ac72db500b3848abd2e63b10ec06777b5be57699
Stjepan Rajko <stjepanr@axosoft.com>
2016-10-18 10:22:52 -0700

    Add support for push_update_reference remote callback

--- generate/templates/manual/include/nodegit.h
+++ generate/templates/manual/include/nodegit.h
@@ -12,4 +12,6 @@ void SetPrivate(v8::Local<v8::Object> object,
                     v8::Local<v8::String> key,
                     v8::Local<v8::Value> value);
 
+typedef int (*git_push_update_reference_cb)(const char *refname, const char *status, void *data);
+
 #endif
